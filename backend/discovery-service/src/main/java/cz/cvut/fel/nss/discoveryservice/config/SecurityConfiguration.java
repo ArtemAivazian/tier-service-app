f9f86ac0bdf7b783ecfc238d3989955b8eb3f797
Denisova, Tatiana <denistat@fel.cvut.cz>
2024-05-03 14:34:23 +0200

    Secure Eureka Dashboard

---
+++ backend/discovery-service/src/main/java/cz/cvut/fel/nss/discoveryservice/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package cz.cvut.fel.nss.discoveryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(httpBasic -> httpBasic
+                        .realmName("DiscoveryRealm")
+                )
+                .build();
+    }
+}
