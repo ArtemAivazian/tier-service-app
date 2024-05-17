72dd0c54d658cad86f05932434173d81df60c0a3
Aivazian, Artem <aivazart@fel.cvut.cz>
2024-05-17 11:54:04 +0200

    Add logging interceptor to the API Gateway

---
+++ backend/api-gateway/src/main/java/cz/cvut/fel/nss/LoggingFilter.java
@@ -0,0 +1,34 @@
+package cz.cvut.fel.nss;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter, Ordered {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        // Log each request header
+        request.getHeaders().forEach((name, values) ->
+                values.forEach(value -> logger.info("Header '{}': {}", name, value)));
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
--- backend/order-service/src/main/java/cz/cvut/fel/nss/config/KafkaTopicConfig.java
+++ backend/order-service/src/main/java/cz/cvut/fel/nss/config/KafkaTopicConfig.java
@@ -1,10 +1,8 @@
 package cz.cvut.fel.nss.config;
 
-import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
